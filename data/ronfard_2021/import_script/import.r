b241554ec2a23576b7fddb3eaf1a2b674bd929a8
kunalhandaUCSB <kunalhanda@ucsb.edu>
2021-07-20 10:55:47 -0700

    added folder and osf data import for ronfard_2021

---
+++ data/ronfard_2021/import_script/import.r
@@ -0,0 +1,31 @@
+library(here)
+library(janitor)
+library(tidyverse)
+library(readxl)
+library(peekds)
+library(osfr)
+
+#TODO: check
+sampling_rate_hz <- 30
+sampling_rate_ms <- 1000/30
+
+dataset_name <- "ronfard_2021"
+read_path <- here("data",dataset_name,"raw_data")
+write_path <- here("data",dataset_name, "processed_data")
+
+dataset_table_filename <- "datasets.csv"
+aoi_table_filename <- "aoi_timepoints.csv"
+subject_table_filename <- "subjects.csv"
+administrations_table_filename <- "administrations.csv"
+stimuli_table_filename <- "stimuli.csv"
+trial_types_table_filename <- "trial_types.csv"
+trials_table_filename <- "trials.csv"
+aoi_regions_table_filename <-  "aoi_region_sets.csv"
+xy_table_filename <-  "xy_timepoints.csv"
+
+osf_token <- read_lines(here("osf_token.txt"))
+peekds::get_raw_data(dataset_name, path = read_path)
+
+remove_repeat_headers <- function(d, idx_var) {
+  d[d[,idx_var] != idx_var,]
+}
