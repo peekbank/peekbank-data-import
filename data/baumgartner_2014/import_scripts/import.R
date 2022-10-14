220db64057b6a7378580dfbbb672b47f8865e838
heidibaum <heidib@stanford.edu>
2022-10-14 14:06:48 -0700

    Update import.R

--- data/baumgartner_2014/import_scripts/import.R
+++ data/baumgartner_2014/import_scripts/import.R
@@ -7,6 +7,10 @@ library(readxl)
 library(peekds)
 library(osfr)
 
+# NOTES: used Adams Marchman import script as starting point/reference
+# PROCEDURE: infants were habituated to 2 novel object/label pairs (e.g,. FribbleA/"lif", FribbleB/"neem"). Then tested in looking while listening paradigm on novel words and familiar words (e.g., baby, doggy, ball). Audio on test trials was presented as "[Target_label]! [Frame][Target_label]? (e.g., Baby! Where's the baby?)
+
+
 ## constants
 sampling_rate_hz <- 30
 sampling_rate_ms <- 1000/30
@@ -37,6 +41,7 @@ remove_repeat_headers <- function(d, idx_var) {
 
 
 # read & organize order files --------------------------------------------------
+# (these are needed because they contain info about label, frame, target side)
 orderpath <- paste(read_path,'/orders/',sep="")
 orderfilescolors <- list.files(path=orderpath, pattern="Colors_order.txt", full.names=TRUE)
 orderfileslabels <- list.files(path=orderpath, pattern="Labels_order.txt", full.names=TRUE)
@@ -57,6 +62,11 @@ ordersspeakers$subject_num <- sub("^", "S", ordersspeakers$subject_num)
 orders <- bind_rows(orderscolors,orderslabels,ordersspeakers)
 orders <- orders[orders$condition %in% c('TestFam', 'TestNov'), ]
 orders <- orders %>% select(-token1,-token2,-token3,-token4,-token5,-token6,-token7)
+colnames(orders)[1] <- "subject"  #rename subject var to match data
+colnames(orders)[3] <- "test_condition"  #rename condition var to match data
+colnames(orders)[4] <- "target_image"  #rename target var to match data
+colnames(orders)[5] <- "target_label"  #rename label var 
+colnames(orders)[6] <- "distractor_image"  #rename distractor var to match data
 
 
 # read raw icoder file
@@ -70,9 +80,11 @@ d_filtered <- d_raw %>%
   select_if(~sum(!is.na(.)) > 0) %>%
   filter(!is.na(`Subject`)) # remove some residual NA rows
 
+
 # Create clean column headers --------------------------------------------------
-d_processed <- d_raw %>%
+d_processed <- d_filtered %>%
   clean_names()
+
 
 # Relabel time bins --------------------------------------------------
 old_names <- colnames(d_processed)
@@ -95,8 +107,22 @@ colnames(d_processed) <- c(metadata_names, pre_dis_names_clean, post_dis_names_c
 # d_processed <- d_processed %>%
 #   select(-all_of(post_dis_names_clean_cols_to_remove))
 
+
+# Merge icoder and order file data --------------------------------------------------
+d_merged <- merge(x = orders, y = d_processed, by = c("subject", "trial", "test_condition", "target_image"))
+
+
+# remove unneeded columns (e.g., flipped R/L stim info from icoder data; keep correct R/L stim info from order df) 
+d_merged <- d_merged %>%
+  select(-l_image, -r_image, -target_side) %>%
+  rename(stimulus_novelty = test_condition)
+
+d_merged$stimulus_novelty <- as.factor(d_merged$stimulus_novelty)
+levels(d_merged$stimulus_novelty) <- c("familiar", "novel")
+
+
 # Convert to long format --------------------------------------------------
-d_tidy <- d_processed %>%
+d_tidy <- d_merged %>%
   pivot_longer(names_to = "t", cols = `-1900`:`5967`, values_to = "aoi")
 
 # recode 0, 1, ., - as distracter, target, other, NA 
@@ -114,19 +140,79 @@ d_tidy <- d_tidy %>%
   mutate(t = as.numeric(t)) # ensure time is an integer/ numeric
 
 
-# Clean up column names and add stimulus information based on existing columnns  ----------------------------------------
-
+# Clean up column names and inclusion/exclusion info based on existing columnns ----------------------------------------
 d_tidy <- d_tidy %>%
   filter(!is.na(subject)) %>%
   select(-condition_trial_num, -vq_rating, -response, -first_shift_gap,-rt) %>%
-  #left-right is from the coder's perspective - flip to participant's perspective
-  mutate(target_side = factor(target_side, levels = c('l','r'), labels = c('right','left'))) %>%
-  rename(left_image = r_image, right_image=l_image) %>%
-  mutate(target_label = target_image) %>%
-  rename(target_image_old = target_image) %>% # since target image doesn't seem to be the specific image identifier
-  mutate(target_image = case_when(target_side == "right" ~ right_image,
-                                  TRUE ~ left_image)) %>%
-  mutate(distractor_image = case_when(target_side == "right" ~ left_image,
-                                      TRUE ~ right_image))
+  rename(exclusion_reason = exclude) %>%
+  rename(included = include_filter)
+  
+# convert exclusion to factor
+d_tidy$exclusion_reason <- as.factor(d_tidy$exclusion_reason)
+levels(d_tidy$exclusion_reason) <- c("included", "< 50% looking on trial", "no looking 1st target window (367-2333ms)", "no looking 2nd target window (4500-5967", "no pre-onset looking")
 
-## left off here ^. need to check what this code is doing before proceeding
+
+# CREATE STIMULUS TABLE ----------------------------------------
+stimulus_table <- d_tidy %>%
+  distinct(target_image,target_label,stimulus_novelty) %>%
+  filter(!is.na(target_image)) %>%
+  mutate(dataset_id = 0,
+         original_stimulus_label = target_label,
+         english_stimulus_label = target_label,
+         stimulus_image_path = "tbd", 
+         # paste0(target_image, ".mov"), # TO DO - update once images are shared/ image file path known
+         image_description = target_label,
+         image_description_source = "test/imageL_imageR.mov",
+         lab_stimulus_id = target_image
+  ) %>%
+  mutate(stimulus_id = seq(0, length(.$lab_stimulus_id) - 1))
+
+
+## add target_id  and distractor_id to d_tidy by re-joining with stimulus table on distactor image
+d_tidy <- d_tidy %>%
+  left_join(stimulus_table %>% select(lab_stimulus_id, stimulus_id, target_label), by=c('target_image' = 'lab_stimulus_id', 'target_label'='target_label')) %>%
+  mutate(target_id = stimulus_id) %>%
+  select(-stimulus_id) %>%
+  left_join(stimulus_table %>% select(lab_stimulus_id, stimulus_id), by=c('distractor_image' = 'lab_stimulus_id')) %>%
+  mutate(distractor_id = stimulus_id) %>%
+  select(-stimulus_id)
+
+# filter out extra distractor rows introduced by previous step (on novel trials, distractor stim ID is determined by target ID)
+d_tidy <- d_tidy %>%
+  filter(!(target_id==1 & distractor_id==9)) %>% # e.g., if target is FribbleA/"lif", distractor is FribbleB/"neem" (stim_id=9 is FribbleB/"lif")
+  filter(!(target_id==2 & distractor_id==7)) %>%
+  filter(!(target_id==7 & distractor_id==2)) %>%
+  filter(!(target_id==9 & distractor_id==1))
+
+# get zero-indexed subject ids 
+d_subject_ids <- d_tidy %>%
+  distinct(subject) %>%
+  mutate(subject_id = seq(0, length(.$subject) - 1))
+#join
+d_tidy <- d_tidy %>%
+  left_join(d_subject_ids, by = "subject")
+
+#get zero-indexed administration ids
+d_administration_ids <- d_tidy %>%
+  distinct(subject_id, subject, months) %>%
+  arrange(subject_id, subject, months) %>%
+  mutate(administration_id = seq(0, length(.$subject_id) - 1)) 
+
+# create zero-indexed ids for trial_types (QUESTION: DOES FRAME MATTER? Martin says yes)
+d_trial_type_ids <- d_tidy %>%
+  #order just flips the target side, so redundant with the combination of target_id, distractor_id, target_side
+  #potentially make distinct based on condition if that is relevant to the study design (no condition manipulation here)
+  distinct(target_id, distractor_id, target_label, targetside, frame) %>%
+  mutate(full_phrase = paste(target_label, frame, target_label)) %>% 
+  mutate(trial_type_id = seq(0, length(target_id) - 1)) 
+
+# joins
+d_tidy_semifinal <- d_tidy %>%
+  left_join(d_administration_ids) %>%
+  left_join(d_trial_type_ids) 
+
+##-----------LEFT OFF HERE----------------
+#get zero-indexed trial ids for the trials table (ISN'T THIS WHAT trial_type_id IS???)
+d_trial_ids <- d_tidy_semifinal %>%
+  distinct(overall_row_number,subject,trial_type_id) %>%
+  mutate(trial_id = seq(0, length(.$trial_type_id) - 1)) 
