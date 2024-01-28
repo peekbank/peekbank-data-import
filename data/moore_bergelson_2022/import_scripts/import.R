cb7bfd6a233a21d430b0580ce646bdd3d231134e
Zhenya Kalenkovich <e.kalenkovich@gmail.com>
2024-01-28 12:03:10 -0500

    save csv's as we go
    In case I introduce unintentional changes.

--- data/moore_bergelson_2022/import_scripts/import.R
+++ data/moore_bergelson_2022/import_scripts/import.R
@@ -74,12 +74,14 @@ cdi_data <-
 # https://docs.google.com/spreadsheets/d/1Z24n9vfrAmEk6_HpoTSh58LnkmSGmNJagguRpI1jwdo/edit#gid=0
 
 ### 1. DATASET TABLE 
-dataset <- tibble(dataset_id = DATASET_ID,
-                  lab_dataset_id = "VNA",
-                  dataset_name = DATASET_NAME,
-                  name = DATASET_NAME, 
-                  shortcite = "Moore & Bergelson (2022)", 
-                  cite = "Moore, C., & Bergelson, E. (2022). Examining the roles of regularity and lexical class in 18–26-month-olds’ representations of how words sound. Journal of Memory and Language, 126, 104337.")
+datasets <- tibble(dataset_id = DATASET_ID,
+                   lab_dataset_id = "VNA",
+                   dataset_name = DATASET_NAME,
+                   name = DATASET_NAME, 
+                   shortcite = "Moore & Bergelson (2022)", 
+                   cite = "Moore, C., & Bergelson, E. (2022). Examining the roles of regularity and lexical class in 18–26-month-olds’ representations of how words sound. Journal of Memory and Language, 126, 104337.")
+
+write_csv(datasets, file = here(output_path, "datasets.csv"))
 
 ### 2. SUBJECTS TABLE
 # Start from the subjects with eyetracking data
@@ -101,6 +103,8 @@ subject_info <- fixations_binned %>%
 
 subjects <- subject_info %>%
   select(subject_id, sex, native_language, lab_subject_id)
+
+write_csv(subjects, file = here(output_path, "subjects.csv"))
 
 ### 3. STIMULI TABLE 
 
@@ -154,6 +158,9 @@ stimuli <-
   arrange(lab_stimulus_id, across(everything())) %>%
   mutate(stimulus_id = 0:(n() - 1))
 
+
+write_csv(stimuli, file = here(output_path,  "stimuli.csv"))
+
 ### 4. ADMINISTRATIONS TABLE 
 
 administrations_aux_data <- subjects %>%
@@ -193,15 +200,21 @@ administrations <- subject_info %>%
   inner_join(administrations_aux_data, by = "subject_id",
              relationship = 'one-to-one')
 
+write_csv(administrations, file = here(output_path, "administrations.csv"))
+
 ### 5. TRIAL TYPES TABLE 
 trial_types <- ... %>%
     select(trial_type_id, full_phrase, full_phrase_language, point_of_disambiguation, 
            target_side, lab_trial_id, condition, aoi_region_set_id, dataset_id, 
            distractor_id, target_id)
 
+write_csv(trial_types, file = here(output_path, "trial_types.csv"))
+
 ### 6. TRIALS TABLE 
 trials <- ... %>%
   select(trial_id, trial_type_id, trial_order)
+
+write_csv(trials, file = here(output_path, "trials.csv"))
 
 ### 7. AOI REGION SETS TABLE
 aoi_region_sets <- tibble(aoi_region_set_id = 0, 
@@ -214,26 +227,36 @@ aoi_region_sets <- tibble(aoi_region_set_id = 0,
                           r_y_max = NA,
                           r_y_min = NA)
 
+write_csv(aoi_region_sets, file = here(output_path, "aoi_region_sets.csv"))
+
+
 ### 8. XY TABLE
 xy_timepoints <- ... %>%
   select(x, y, t, point_of_disambiguation, administration_id, trial_id) %>%
   peekds::resample_times(table_type = "xy_timepoints") 
+
+write_csv(xy_timepoints, file = here(output_path, "xy_timepoints.csv"))
   
 ### 9. AOI TIMEPOINTS TABLE
 aoi_timepoints <- ... %>%
   select(aoi, t, point_of_disambiguation, administration_id, trial_id) %>%
   peekds::resample_times(table_type = "aoi_timepoints") 
 
-################## WRITING AND VALIDATION ##################
-write_csv(dataset, file = here(output_path, "datasets.csv"))
-write_csv(subjects, file = here(output_path, "subjects.csv"))
-write_csv(stimuli, file = here(output_path,  "stimuli.csv"))
-write_csv(administrations, file = here(output_path, "administrations.csv"))
-write_csv(trial_types, file = here(output_path, "trial_types.csv"))
-write_csv(trials, file = here(output_path, "trials.csv"))
-write_csv(aoi_region_sets, file = here(output_path, "aoi_region_sets.csv"))
-write_csv(xy_timepoints, file = here(output_path, "xy_timepoints.csv"))
 write_csv(aoi_timepoints, file = here(output_path, "aoi_timepoints.csv"))
+
+################## WRITING AND VALIDATION ##################
+
+# TODO: Uncomment write_csv's below and delete all the ones above once finished
+# write_csv(dataset, file = here(output_path, "datasets.csv"))
+# write_csv(subjects, file = here(output_path, "subjects.csv"))
+# write_csv(stimuli, file = here(output_path,  "stimuli.csv"))
+# write_csv(administrations, file = here(output_path, "administrations.csv"))
+# write_csv(trial_types, file = here(output_path, "trial_types.csv"))
+# write_csv(trials, file = here(output_path, "trials.csv"))
+# write_csv(aoi_region_sets, file = here(output_path, "aoi_region_sets.csv"))
+# write_csv(xy_timepoints, file = here(output_path, "xy_timepoints.csv"))
+# write_csv(aoi_timepoints, file = here(output_path, "aoi_timepoints.csv"))
+
 
 # run validator
 peekds::validate_for_db_import(dir_csv = output_path)
