4405c69bf9c759d63ebb7c880edad8c66f98c35a
Adrian Steffan <adrian.steffan@hotmail.de>
2024-05-23 13:12:41 +0200

    tiny fixes for weisleder stl, prescreen exclusions are fine

--- data/weisleder_stl/import_script/import.R
+++ data/weisleder_stl/import_script/import.R
@@ -14,6 +14,8 @@ dataset_name = "weisleder_stl"
 read_path <- here("data" ,dataset_name,"raw_data")
 write_path <- here("data",dataset_name, "processed_data")
 
+dir.create(write_path, showWarnings = FALSE)
+
 dataset_table_filename <- "datasets.csv"
 aoi_table_filename <- "aoi_timepoints.csv"
 subject_table_filename <- "subjects.csv"
@@ -23,7 +25,7 @@ trials_table_filename <- "trials.csv"
 trial_types_table_filename <- "trial_types.csv"
 aoi_regions_table_filename <-  "aoi_region_sets.csv"
 xy_table_filename <-  "xy_timepoints.csv"
-osf_token <- read_lines(here("osf_token.txt"))
+#osf_token <- read_lines(here("osf_token.txt"))
 
 
 #### FUNCTIONS FOR PREPROCESSING ####
