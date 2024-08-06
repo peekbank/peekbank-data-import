1dcfdecff0c75727dc006ce928d5e176a1087c83
Adrian Steffan <adrian.steffan@hotmail.de>
2024-08-06 18:21:10 +0200

    Add draft with initial thoughts about idless import

---
+++ helper_functions/idless_draft.R
@@ -0,0 +1,86 @@
+# important note: the digest function will not take aux data as input.
+# Rather, it will output empty aux data columns that can be populated manually
+
+# TODO: where to represent cdi requirement?
+# TODO: offer a helper to handle cdi data (take a dataframe as input)
+
+digest.dataset <- function(
+    dataset_name,
+    lab_dataset_id = NA,
+    cite,
+    shortcite,
+    wide.table,
+    ){
+  
+  required_cols <- c(
+    "subject_id",
+    "sex",
+    "native_language",
+    "age",
+    "age_units",
+    "t_norm",
+    "x",
+    "y",
+    "aoi",
+    "l_x_max",
+    "l_x_min",
+    "l_y_max",
+    "l_y_min",
+    "r_x_max",
+    "r_x_min",
+    "r_y_max",
+    "r_y_min",
+    "full_phrase",
+    "full_phrase_language",
+    "point_of_disambiguation",
+    "target_side",
+    "trial_name",
+    "condition",
+    "vanilla_trial",
+    "trial_order",
+    "excluded",
+    "exclusion_reason",
+    "experiment_id", # to distinguish administrations
+    "monitor_size_x",
+    "monitor_size_y",
+    "sample_rate",
+    "tracker",
+    "coding_method",
+    "target_stimulus_label_original",
+    "target_stimulus_label_english",
+    "target_stimulus_novelty",
+    "target_stimulus_image_path",
+    "target_image_description",
+    "target_image_description_source",
+    "distractor_stimulus_label_original",
+    "distractor_stimulus_label_english",
+    "distractor_stimulus_novelty",
+    "distractor_stimulus_image_path",
+    "distractor_image_description",
+    "distractor_image_description_source"
+  )
+  
+  missing_cols <- setdiff(required_cols, colnames(wide.table))
+  if(length(missing_cols) > 0){
+    print("Some columns are missing from the input table:")
+    print(missing_cols)
+    stop()
+  }
+  
+  # TODO Validate the values
+  
+  # TODO create the tables and ids (Rename & split)
+  
+  
+  return(list(
+    datasets = datasets,
+    subjects = subjects,
+    administrations = administrations,
+    stimuli = stimuli,
+    trial_types = trial_types,
+    trials = trials,
+    aoi_timepoints = aoi_timepoints,
+    xy_timepoints = xy_timepoints,
+    aoi_region_sets = aoi_region_sets,
+  ))
+}
