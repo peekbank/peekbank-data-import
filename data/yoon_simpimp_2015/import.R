292bea808fd621b46b718e83d5ed0c9fac6c27b9
vboyce <vero.boyce@gmail.com>
2024-08-30 11:46:37 -0700

    trial level exclusions

--- data/yoon_simpimp_2015/import.R
+++ data/yoon_simpimp_2015/import.R
@@ -28,27 +28,21 @@ generate_aois <- function(x, y, target_side,
 }
 # 
 eyetracking_path <- here(data_path, "eyetracking")
-data_ex_1<- read_csv(here(eyetracking_path, "eyetrack_expt1.csv")) |> 
-  mutate(expt="0")
+data_ex_1<- read_csv(here(eyetracking_path, "eyetrack_expt1.csv")) |> select(-expt)
 
-data_ex_2 <- read_csv(here(eyetracking_path, "eyetrack_expt2.csv")) |> 
-  mutate(expt="scale") 
+data_ex_2 <- read_csv(here(eyetracking_path, "eyetrack_expt2.csv")) |> select(-expt)
 
 exclusion_data <- read_csv(here(eyetracking_path, "simpimp_et_log.csv")) |> 
   filter(age!="adult")
 order_data <- read_csv(here(eyetracking_path, "simpimp_et_order.csv"))
 
-draft_data <- data_ex_1 |> bind_rows(data_ex_2) |> inner_join(order_data) |> 
-  #mutate(time=ifelse(is.na(order), NA, time), #this is janky - VB
-  #       stimulus=ifelse(is.na(order), NA, stimulus)) |> 
-  #group_by(subid) |> 
-  #     fill(order:targetOnset) |> 
-  #     fill(time:stimulus) |> 
+draft_data <- data_ex_1 |> bind_rows(data_ex_2) |> inner_join(order_data) |>
   ungroup() |> 
   filter(!is.na(time)) |> 
   mutate(t=t-time,
          point_of_disambiguation=1000*targetOnset) |> 
-  select(-lx, -ly, -rx, -ry) |> inner_join(exclusion_data) |> 
+  select(-lx, -ly, -rx, -ry) |> 
+  inner_join(exclusion_data) |> 
   mutate(target_side=ifelse(targetPos=="R","right", "left"),
          condition=case_when(
            trial_type=="cs" ~ "control-single",
@@ -57,15 +51,13 @@ draft_data <- data_ex_1 |> bind_rows(data_ex_2) |> inner_join(order_data) |>
          ),
          vanilla_trial = FALSE, # there are decisions to be made about whether
          # the cs trials are vanilla, we're leaning not, but shrug
-         excluded = keep_drop=="drop", # note there should also be trial level exclusions, but we haven't tracked those down
+         excluded = keep_drop=="drop", # note there should also be trial level exclusions
+         # which we have now tracked down and apply later
          exclusion_reason = ifelse(keep_drop=="drop", "participant level some reason", NA),
          target_stimulus_label_original = str_c("target_",condition, "_",item),
          distractor_stimulus_label_original=str_c("distractor_", condition,"_", item)
          # will need to fix this
-  )
-
-
-wide.table <- draft_data |> 
+  )|> 
   mutate(age_units="years",
          age=as.numeric(age),
          full_phrase=NA,
@@ -75,7 +67,7 @@ wide.table <- draft_data |>
          sample_rate=NA,
          tracker = NA,
          coding_method = "eyetracking",
-         # note we will need to figure this out off of item at some point
+         # note we will need to figure out all of the following off of item at some point
          target_stimulus_label_english = target_stimulus_label_original,
          target_stimulus_novelty = "familiar",
          target_stimulus_image_path = "stimulus_image_path",
@@ -94,7 +86,6 @@ wide.table <- draft_data |>
   age = age,
   age_units,
   t,
-  #aoi = NA,
   full_phrase,
   full_phrase_language,
   point_of_disambiguation,
@@ -120,7 +111,9 @@ wide.table <- draft_data |>
   distractor_image_description,
   distractor_image_description_source,
   x,
-  y
+  y,
+  stimulus,
+  expt
 ) %>%
   # optional 
   mutate(
@@ -149,46 +142,64 @@ wide.table <- draft_data |>
                                 l_x_max, l_x_min, l_y_max, l_y_min,
                                 r_x_max, r_x_min, r_y_max, r_y_min))
 
-    
+# trials are included if >50% of time points are valid
+trial_include <- draft_data |> group_by(subject_id, stimulus) |> 
+  summarize(missing_pct=sum(!aoi %in% c("target","distractor"))/n()) |> 
+  filter(missing_pct<.5) |> mutate(good_trial=T)
+
+#subjects are included if >50% of trial (8+) are valid
+sub_include <- trial_include |> group_by(subject_id) |> tally() |> filter(n>7) |> 
+  mutate(good_subject=T)
   
+wide.table <- draft_data |> left_join(trial_include) |> left_join(sub_include) |> 
+  mutate(exclusion_reason=case_when(
+    !is.na(exclusion_reason) ~ exclusion_reason, 
+    is.na(age) ~ "out of age range",
+    is.na(good_trial) ~ "trial has too little valid looking",
+    is.na(good_subject) ~ "participant has too few valid trials",
+    T ~ NA
+  ),
+  excluded=ifelse(is.na(exclusion_reason), F, T)) |> 
+  select(-stimulus, -good_trial, -good_subject)
+  
+  
+
+  
+
 dataset_list <- digest.dataset(
   dataset_name = dataset_name,
   lab_dataset_id = NA,
   cite = "Yoon, E. J., Wu, Y. C., Frank, M. C. (2015). Children's Online Processing of Ad-Hoc Implicatures. Proceedings of the 37th Annual Conference of the Cognitive Science Society.",
   shortcite = "Yoon & Frank (2015)",
   wide.table = wide.table,
-  rezero=T,
+  rezero=F,
   resample=T,
-  normalize=T
+  normalize=F
 )
 
 write_and_validate_list(dataset_list, cdi_expected = FALSE, upload=FALSE)
-
-wide.table  |> mutate(t_round=round(t/100)*100) |> 
-  filter(condition %in% c("control-single", "control-double")) |> 
-  filter(age>3) |> 
-  group_by(target_stimulus_label_original, t_round) |> 
-  summarize(accuracy=sum(aoi=="target")/
-              (sum(aoi=="target")+sum(aoi=="distractor"))) |> 
-  ggplot(aes(x=t_round, y=accuracy, color=target_stimulus_label_original))+geom_line(alpha=.1)+
-  geom_hline(yintercept=.5)+geom_vline(xintercept=2000)
-
-wide.table  |> mutate(offset=t-point_of_disambiguation,
-                      t_round=round(offset/100)*100) |> 
-  #filter(condition %in% c("control-single", "control-double")) |> 
-  #filter(age>3) |> 
-  group_by(target_stimulus_label_original, t_round) |> 
-  summarize(accuracy=sum(aoi=="target")/
-              (sum(aoi=="target")+sum(aoi=="distractor"))) |> 
-  ggplot(aes(x=t_round, y=accuracy, color=target_stimulus_label_original))+geom_line(alpha=.1)+
-  geom_hline(yintercept=.5)+geom_vline(xintercept=0)+geom_vline(xintercept=300)+theme(legend.position="none")
-
-dataset_list$aoi_timepoints |> left_join(dataset_list$administrations) |> 
-  mutate(t_round=round(t_norm/100)*100) |> 
-  #filter(condition %in% c("control-single", "control-double")) |> 
-  #filter(age>3) |> 
-  group_by(administration_id, t_round) |> 
-  summarize(accuracy=sum(aoi=="target")/
-              (sum(aoi=="target")+sum(aoi=="distractor"))) |> 
-  ggplot(aes(x=t_round, y=accuracy, color=as.character(administration_id)))+geom_line(alpha=.1)+
-  geom_hline(yintercept=.5)+geom_vline(xintercept=0)+geom_vline(xintercept=300)+theme(legend.position="none")
+# 
+# wide.table  |> mutate(t_round=round(t/100)*100) |> 
+#   filter(condition %in% c("control-single", "control-double")) |> 
+#   filter(age>3) |> 
+#   group_by(target_stimulus_label_original, t_round) |> 
+#   summarize(accuracy=sum(aoi=="target")/
+#               (sum(aoi=="target")+sum(aoi=="distractor"))) |> 
+#   ggplot(aes(x=t_round, y=accuracy, color=target_stimulus_label_original))+geom_line(alpha=.1)+
+#   geom_hline(yintercept=.5)+geom_vline(xintercept=2000)
+# 
+# wide.table  |> mutate(offset=t-point_of_disambiguation,
+#                       t_round=round(offset/100)*100) |> 
+#   group_by(subject_id, t_round) |> 
+#   summarize(accuracy=sum(aoi=="target")/
+#               (sum(aoi=="target")+sum(aoi=="distractor"))) |> 
+#   ggplot(aes(x=t_round, y=accuracy, color=subject_id))+geom_line(alpha=.1)+
+#   geom_hline(yintercept=.5)+geom_vline(xintercept=0)+geom_vline(xintercept=300)+theme(legend.position="none")
+# 
+# dataset_list$aoi_timepoints |> left_join(dataset_list$administrations) |> 
+#   mutate(t_round=round(t_norm/100)*100) |> 
+#   group_by(administration_id, t_round) |> 
+#   summarize(accuracy=sum(aoi=="target")/
+#               (sum(aoi=="target")+sum(aoi=="distractor"))) |> 
+#   ggplot(aes(x=t_round, y=accuracy, color=as.character(administration_id)))+geom_line(alpha=.1)+
+#   geom_hline(yintercept=.5)+geom_vline(xintercept=0)+geom_vline(xintercept=300)+theme(legend.position="none")
