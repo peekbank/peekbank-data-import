36203bf6bbe879b0d517031c95c4bfdf8fc75852
mzettersten <mzettersten@users.noreply.github.com>
2024-09-02 17:48:15 -0700

    fixing trial_id creation which caused problems for resampling

--- data/yoon_simpimp_2015/import.R
+++ data/yoon_simpimp_2015/import.R
@@ -36,10 +36,25 @@ exclusion_data <- read_csv(here(eyetracking_path, "simpimp_et_log.csv")) |>
   filter(age!="adult")
 order_data <- read_csv(here(eyetracking_path, "simpimp_et_order.csv"))
 
-draft_data <- data_ex_1 |> bind_rows(data_ex_2) |> inner_join(order_data) |>
+combined_data <- data_ex_1 |> 
+  bind_rows(data_ex_2) |> 
+  inner_join(order_data) |>
+  ungroup()
+
+#add trial index based on change in stimulus
+combined_data_index <- combined_data |> 
+  group_by(subid) |> 
+  mutate(
+    change = stimulus != lag(stimulus, default = first(stimulus)), # Identify changes
+    trial_index = cumsum(change) # Create a running index
+  ) |> 
   ungroup() |> 
+  select(-change)
+
+draft_data <- combined_data_index |> 
   filter(!is.na(time)) |> 
-  mutate(t=t-time,
+  mutate(
+    #t=t-time,
          point_of_disambiguation=1000*targetOnset) |> 
   select(-lx, -ly, -rx, -ry) |> 
   inner_join(exclusion_data) |> 
@@ -113,7 +128,8 @@ draft_data <- data_ex_1 |> bind_rows(data_ex_2) |> inner_join(order_data) |>
   x,
   y,
   stimulus,
-  expt
+  expt,
+  trial_index
 ) %>%
   # optional 
   mutate(
@@ -132,7 +148,7 @@ draft_data <- data_ex_1 |> bind_rows(data_ex_2) |> inner_join(order_data) |>
     monitor_size_y = 1050,
     # if two subsequent trials can have the same stimuli combination,
     # use this to indicate the trial order within an administration
-    trial_index = NA,
+    # trial_index = NA,
     # lab specific name for trials
     trial_name = NA,
     # lab specific names for stimuli
@@ -162,44 +178,43 @@ wide.table <- draft_data |> left_join(trial_include) |> left_join(sub_include) |
   excluded=ifelse(is.na(exclusion_reason), F, T)) |> 
   select(-stimulus, -good_trial, -good_subject)
   
-  
-
-  
-
 dataset_list <- digest.dataset(
   dataset_name = dataset_name,
   lab_dataset_id = NA,
   cite = "Yoon, E. J., Wu, Y. C., Frank, M. C. (2015). Children's Online Processing of Ad-Hoc Implicatures. Proceedings of the 37th Annual Conference of the Cognitive Science Society.",
   shortcite = "Yoon & Frank (2015)",
   wide.table = wide.table,
-  rezero=F,
+  rezero=T,
   resample=T,
-  normalize=F
+  normalize=T
 )
 
 write_and_validate_list(dataset_list, cdi_expected = FALSE, upload=FALSE)
-# 
-# wide.table  |> mutate(t_round=round(t/100)*100) |> 
-#   filter(condition %in% c("control-single", "control-double")) |> 
-#   filter(age>3) |> 
-#   group_by(target_stimulus_label_original, t_round) |> 
-#   summarize(accuracy=sum(aoi=="target")/
-#               (sum(aoi=="target")+sum(aoi=="distractor"))) |> 
-#   ggplot(aes(x=t_round, y=accuracy, color=target_stimulus_label_original))+geom_line(alpha=.1)+
-#   geom_hline(yintercept=.5)+geom_vline(xintercept=2000)
-# 
-# wide.table  |> mutate(offset=t-point_of_disambiguation,
-#                       t_round=round(offset/100)*100) |> 
-#   group_by(subject_id, t_round) |> 
-#   summarize(accuracy=sum(aoi=="target")/
-#               (sum(aoi=="target")+sum(aoi=="distractor"))) |> 
-#   ggplot(aes(x=t_round, y=accuracy, color=subject_id))+geom_line(alpha=.1)+
-#   geom_hline(yintercept=.5)+geom_vline(xintercept=0)+geom_vline(xintercept=300)+theme(legend.position="none")
-# 
-# dataset_list$aoi_timepoints |> left_join(dataset_list$administrations) |> 
-#   mutate(t_round=round(t_norm/100)*100) |> 
-#   group_by(administration_id, t_round) |> 
-#   summarize(accuracy=sum(aoi=="target")/
-#               (sum(aoi=="target")+sum(aoi=="distractor"))) |> 
-#   ggplot(aes(x=t_round, y=accuracy, color=as.character(administration_id)))+geom_line(alpha=.1)+
-#   geom_hline(yintercept=.5)+geom_vline(xintercept=0)+geom_vline(xintercept=300)+theme(legend.position="none")
+
+#checking resampling behavior
+temp <- wide.table  |> 
+  group_by(subject_id,trial_index) |> 
+  mutate(t_zeroed = t - t[1]) |> 
+  ungroup() |> 
+  mutate(
+    offset=t_zeroed-point_of_disambiguation,
+    t_round=round(offset/100)*100) |>
+  group_by(subject_id, t_round) |>
+  summarize(accuracy=sum(aoi=="target")/
+              (sum(aoi=="target")+sum(aoi=="distractor"))) 
+temp |>
+  ggplot(aes(x=t_round, y=accuracy, color=as.character(subject_id)))+geom_line(alpha=.1)+
+  geom_smooth(color="black")+
+  geom_hline(yintercept=.5)+geom_vline(xintercept=0)+geom_vline(xintercept=300)+theme(legend.position = "none")
+
+temp_resampled <- dataset_list$aoi_timepoints |> left_join(dataset_list$administrations) |>
+  #mutate(t_round=round(t_norm/100)*100) |>
+  ungroup() |> 
+  group_by(administration_id, t_norm) |>
+  summarize(N=n(),
+            accuracy=sum(aoi=="target")/
+              (sum(aoi=="target")+sum(aoi=="distractor"))) 
+temp_resampled |>
+  ggplot(aes(x=t_norm, y=accuracy, color=as.character(administration_id)))+geom_line(alpha=.1)+
+  geom_smooth(color="black")+
+  geom_hline(yintercept=.5)+geom_vline(xintercept=0)+geom_vline(xintercept=300)+theme(legend.position="none")
