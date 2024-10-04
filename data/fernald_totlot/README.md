63702529f428a5a45b723bca5fa2b4a434386339
mzettersten <mzettersten@users.noreply.github.com>
2024-10-04 12:09:21 -0700

    progress on totlot import

---
+++ data/fernald_totlot/README.md
@@ -0,0 +1,24 @@
+# fernald_totlot readme
+
+1. **Reference.**
+Fernald, A., Perfors, A., & Marchman, V. A. (2006). Picking up speed in understanding: Speech processing efficiency and vocabulary growth across the 2nd year.Developmental Psychology, 42(1), 98–116.
+
+Contributors: Virginia Marchman
+
+2. **Abstract.**
+
+To explore how online speech processing efficiency relates to vocabulary growth in the 2nd year, the authors longitudinally observed 59 English-learning children at 15, 18, 21, and 25 months as they looked at pictures while listening to speech naming one of the pictures. The time course of eye movements in response to speech revealed significant increases in the efficiency of comprehension over this period. Further, speed and accuracy in spoken word recognition at 25 months were correlated with measures of lexical and grammatical development from 12 to 25 months. Analyses of growth curves showed that children who were faster and more accurate in online comprehension at 25 months were those who showed faster and more accelerated growth in expressive vocabulary across the 2nd year.
+
+3. **Original study info.**
+
+
+4. **Importing decisions.**
+
+- data truncated ~ 3833 post target word onset because very little data was coded in subsequent columns
+- word_onset column was set as F0 (i.e. the word onset column in the canonical icoder format)
+- no pre-word onset coded data seems to be available
+
+5. **Importing ambiguity.**
+
+- 
+
--- data/fernald_totlot/import.R
+++ data/fernald_totlot/import.R
@@ -1,10 +1,99 @@
 ## 1. Initial Setup
 library(here)
+library(janitor)
 
 source(here("helper_functions", "idless_draft.R"))
 source(here("helper_functions", "common.R"))
 dataset_name <- "fernald_totlot"
 data_path <- init(dataset_name)
+
+data_folder <- here(data_path)
+
+sampling_rate_ms <- 1000 / 30
+
+#### process 15-mo-old data into long format ####
+d_raw_15 <- read_csv(here(data_path, "originalTL15vmclean.csv")) %>%
+  mutate(age_group="15 months") %>%
+  rename(F0 = word_onset) %>%
+  relocate("filter_$","tacc1800","rtmsec", .after="targetper") %>%
+  # filter columns with all NAs
+  select_if(~ sum(!is.na(.)) > 0) 
+
+#rename the last columns
+colnames(d_processed_15)[str_detect(colnames(d_processed_15), "V\\d")] <- c(
+  "F3233","F3267","F3300","F3333","F3367","F3400","F3433","F3467","F3500","F3533","F3567","F3600","F3633",
+  "F3667","F3700","F3733","F3767","F3800","F3833","F3867","F3900","F3933","F3967","F4000","F4033","F4067","F4100")
+
+#clean names
+d_processed_15 <- d_processed_15 %>%
+  clean_names()
+
+#relabel time bins
+old_names_15 <- colnames(d_processed_15)
+metadata_names <- old_names_15[!str_detect(old_names_15, "f\\d")]
+post_dis_names <- old_names_15[str_detect(old_names_15, "f\\d")]
+post_dis_names_clean <- post_dis_names %>% str_remove("f")
+
+colnames(d_processed_15) <- c(metadata_names, post_dis_names_clean)
+
+### truncate columns at F3833, since trials are almost never coded later than this timepoint
+## TO DO: note decision in ReadMe
+post_dis_names_clean_cols_to_remove <- post_dis_names_clean[117:length(post_dis_names_clean)]
+# remove
+d_processed_15 <- d_processed_15 %>%
+  select(-all_of(post_dis_names_clean_cols_to_remove))
+
+# Convert to long format
+d_tidy_15 <- d_processed_15 %>%
+  pivot_longer(names_to = "t", cols = `0`:`3833`, values_to = "aoi")
+
+
+#### process 18-mo-old data into long format ####
+d_raw_18 <- read_csv(here(data_path,"originalTL18vm.csv")) %>%
+  mutate(age_group="18 months") %>%
+  rename(F0 = word_onset) %>%
+  relocate("filter_$","tacc1800","rtmsec", .after="targetper") %>%
+  # filter columns with all NAs
+  select_if(~ sum(!is.na(.)) > 0)
+
+#combine
+d_tidy <- bind_rows(d_processed_15,d_processed_18)
+
+# recode 0, 1, ., - as distracter, target, other, NA [check in about this]
+# this leaves NA as NA
+d_tidy <- d_tidy %>%
+rename(aoi_old = aoi) %>%
+  mutate(aoi = case_when(
+    aoi_old == "0" ~ "distractor",
+    aoi_old == "1" ~ "target",
+    aoi_old == "0.5" ~ "other",
+    aoi_old == "." ~ "missing",
+    aoi_old == "-" ~ "missing",
+    is.na(aoi_old) ~ "missing"
+  )) %>%
+  mutate(t = as.numeric(t)) # ensure time is an integer/ numeric
+
+#quick summary
+subj <- d_tidy_15 %>%
+  group_by(subj,t) %>%
+  summarize(
+    mean_looking=mean(as.numeric(aoi_old),na.rm=T)
+  )
+
+overall <- subj %>%
+  group_by(t) %>%
+  summarize(
+    avg = mean(mean_looking)
+  )
+
+ggplot(overall, aes(t,avg)) +
+  geom_hline(yintercept=0.5,linetype="dashed")+
+  geom_line(data=subj,aes(y=mean_looking,group=as.factor(subj)),alpha=0.05)+
+  theme(legend.position="none")+
+  geom_line()
+  
+
+
 
 ## 2. Creating the wide.table
 # Populate your wide table from the raw data here
@@ -71,9 +160,9 @@ wide.table <- tibble(
 
 dataset_list <- digest.dataset(
   dataset_name = dataset_name,
-  lab_dataset_id = NA,
-  cite = "TODO",
-  shortcite = "TODO",
+  lab_dataset_id = "totlot",
+  cite = "Fernald, A., Perfors, A., & Marchman, V. A. (2006). Picking up speed in understanding: Speech processing efficiency and vocabulary growth across the 2nd year.Developmental Psychology, 42(1), 98–116.",
+  shortcite = "Fernald et al. (2006)",
   wide.table = wide.table,
   rezero=TRUE,
   normalize=TRUE,
