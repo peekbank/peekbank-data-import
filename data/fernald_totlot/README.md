268b0a359ba8e057bc5f48aadb5a68bfc17989c1
Adrian Steffan <adrian.steffan@hotmail.de>
2024-10-23 17:33:25 +0200

    first working version of fernald_totlot - still needs some discussion (cutoffs, vanilla, cdi, stimuli)

--- data/fernald_totlot/README.md
+++ data/fernald_totlot/README.md
@@ -11,10 +11,32 @@ To explore how online speech processing efficiency relates to vocabulary growth 
 
 3. **Original study info.**
 
+TODO
+
+Relevant excerpt regarding stimulus novelty / trial vanillaness:
+
+The speech stimuli at all ages consisted of prerecorded sentences containing target words in
+a familiar carrier phrase such as “Where’s the [target]? Do you see it?” At 15 months, the
+four target words (doggie, baby, ball, shoe) used by Fernald et al. (1998) were each
+presented six times. In addition to these 24 trials, 6 filler trials were included to increase the
+variability of the stimulus set. At 18 months, the target words were doggie, baby, ball, and
+car, each presented twice as whole words and twice as truncated words (daw, bei, baw, ka).
+This manipulation was motivated by the finding that the ability to recognize familiar words
+using partial phonetic information develops between 18 and 21 months of age and is related
+to level of lexical development (Fernald et al., 2001). In addition to these 16 trials, 13 filler
+trials were included.2 At 21 months, doggie, baby, birdie, and kitty, were presented both as
+whole words and as truncated words (daw, bei, ber, ki) for a total of 16 trials. On 8
+additional trials, the 21-month-olds also heard juice and cookie as target words, preceded by
+frames in which the verb was either semantically related (drink, eat) or semantically neutral
+(take, look at), with four filler trials. At 25 months, the standard set of target words (doggie,
+baby, ball, car) was augmented by target words typically learned later in the 2nd year
+(monkey, cow, flower, tree, animal), presented as whole words. To diversify the sentence
+frames, we included adjectives (nice, pretty) before target words on 8 trials. Four filler trials
+were included.
+
 
 4. **Importing decisions.**
 
-- data truncated ~ 3833 post target word onset because very little data was coded in subsequent columns
 - word_onset column was set as F0 (i.e. the word onset column in the canonical icoder format)
 - no pre-word onset coded data seems to be available
 
--- data/fernald_totlot/import.R
+++ data/fernald_totlot/import.R
@@ -1,6 +1,6 @@
-## 1. Initial Setup
 library(here)
 library(janitor)
+library(readxl)
 
 source(here("helper_functions", "idless_draft.R"))
 source(here("helper_functions", "common.R"))
@@ -9,182 +9,127 @@ data_path <- init(dataset_name)
 
 data_folder <- here(data_path)
 
-sampling_rate_ms <- 1000 / 30
+sampling_rate_hz <- 30
+sampling_rate_ms <- 1000 / sampling_rate_hz
 
-#### process 15-mo-old data into long format ####
-d_raw_15 <- read_csv(here(data_path, "originalTL15vmclean.csv")) 
+rename_frame_columns <- function(df) {
+  f_cols <- str_extract(colnames(df), "(?<=F)\\d+")
+  last_framerate <- max(as.numeric(f_cols[!is.na(f_cols)]))
 
-d_processed_15 <- d_raw_15 %>%
-  mutate(age_group="15 months") %>%
-  rename(F0 = word_onset) %>%
-  relocate("filter_$","tacc1800","rtmsec","age_group", .after="targetper") %>%
-  # filter columns with all NAs
-  select_if(~ sum(!is.na(.)) > 0) 
+  # handle xx3 and xx7 edge cases - really hacky but works for this dataset/framerate
+  last_framerate <- last_framerate +
+    if (last_framerate %% 10 == 3) 0.33 else (if (last_framerate %% 10 == 7) -0.33 else 0)
 
-#rename the last columns
-colnames(d_processed_15)[str_detect(colnames(d_processed_15), "V\\d")] <- c(
-  "F3233","F3267","F3300","F3333","F3367","F3400","F3433","F3467","F3500","F3533","F3567","F3600","F3633",
-  "F3667","F3700","F3733","F3767","F3800","F3833","F3867","F3900","F3933","F3967","F4000","F4033","F4067","F4100")
+  # only get V columns that come after first F column
+  v_cols <- which(str_detect(colnames(df), "V\\d"))
+  v_cols <- v_cols[v_cols > min(which(str_detect(colnames(df), "^F\\d+")))]
 
-#clean names
-d_processed_15 <- d_processed_15 %>%
-  clean_names()
+  colnames(df)[v_cols] <- paste0("F", round(seq(
+    from = last_framerate + sampling_rate_ms,
+    by = sampling_rate_ms,
+    length.out = length(v_cols)
+  )))
 
-#relabel time bins
-old_names_15 <- colnames(d_processed_15)
-metadata_names_15 <- old_names_15[!str_detect(old_names_15, "f\\d")]
-post_dis_names_15 <- old_names_15[str_detect(old_names_15, "f\\d")]
-post_dis_names_clean_15 <- post_dis_names_15 %>% str_remove("f")
+  return(df)
+}
 
-colnames(d_processed_15) <- c(metadata_names_15, post_dis_names_clean_15)
-
-### truncate columns at F3833, since trials are almost never coded later than this timepoint
-## TO DO: note decision in ReadMe
-post_dis_names_clean_cols_to_remove_15 <- post_dis_names_clean_15[117:length(post_dis_names_clean_15)]
-# remove
-d_processed_15 <- d_processed_15 %>%
-  select(-all_of(post_dis_names_clean_cols_to_remove_15))
-
-# Convert to long format
-d_tidy_15 <- d_processed_15 %>%
-  pivot_longer(names_to = "t", cols = `0`:`3833`, values_to = "aoi")
-
-#### process 18-mo-old data into long format ####
-d_raw_18 <- read_csv(here(data_path,"originalTL18vm.csv"))
-
-d_processed_18 <- d_raw_18 %>%
-  mutate(age_group="18 months") %>%
-  rename(F0 = word_onset) %>%
-  relocate("filter_$","tacc1800","rtmsec","age_group", .after="targetper") %>%
-  # filter columns with all NAs
-  select_if(~ sum(!is.na(.)) > 0)
-
-#rename the last columns
-colnames(d_processed_18)[str_detect(colnames(d_processed_18), "V\\d")] <- c(
-  "F3167","F3200","F3233")
-
-#clean names
-d_processed_18 <- d_processed_18 %>%
-  clean_names()
-
-#relabel time bins
-old_names_18 <- colnames(d_processed_18)
-metadata_names_18 <- old_names_18[!str_detect(old_names_18, "f\\d")]
-post_dis_names_18 <- old_names_18[str_detect(old_names_18, "f\\d")]
-post_dis_names_clean_18 <- post_dis_names_18 %>% str_remove("f")
-
-colnames(d_processed_18) <- c(metadata_names_18, post_dis_names_clean_18)
-
-### truncate columns at F3133, since trials are almost never coded later than this timepoint
-## TO DO: note decision in ReadMe
-post_dis_names_clean_cols_to_remove_18 <- post_dis_names_clean_18[121:length(post_dis_names_clean_18)]
-# remove
-d_processed_18 <- d_processed_18 %>%
-  select(-all_of(post_dis_names_clean_cols_to_remove_15))
-
-# Convert to long format
-d_tidy_18 <- d_processed_18 %>%
-  pivot_longer(names_to = "t", cols = `0`:`3133`, values_to = "aoi")
+read_age_group <- function(age_group, filename) {
+  df <- here(data_path, filename) %>%
+    read_csv() %>%
+    mutate(age_group = age_group) %>%
+    rename(F0 = word_onset) %>%
+    relocate("filter_$", "tacc1800", "rtmsec", "age_group", .after = "targetper") %>%
+    # filter columns with all NAs
+    select_if(~ sum(!is.na(.)) > 0) %>%
+    rename_frame_columns() %>%
+    clean_names() %>%
+    relocate(matches("^f\\d+"), .after = last_col())
+  # relabel frame bins
+  colnames(df) <- sub("^f(\\d+)", "\\1", colnames(df))
+  x <- df %>%
+    pivot_longer(names_to = "t", cols = `0`:last_col(), values_to = "aoi")
+}
 
 
-#combine
-d_tidy <- bind_rows(d_tidy_15,d_tidy_18)
+# TODO previous cutoff frames that we dont do anymore, should we reintroduce them?:
+# 15: 3833
+# 18: 3133
 
-# recode 0, 1, ., - as distracter, target, other, NA [check in about this]
-# this leaves NA as NA
-d_tidy <- d_tidy %>%
-rename(aoi_old = aoi) %>%
+# combine
+d_tidy <- bind_rows(
+  read_age_group(15, "originalTL15vmclean.csv"),
+  read_age_group(18, "originalTL18vm.csv"),
+  read_age_group(21, "originalTL21vm.csv"),
+  read_age_group(25, "originalTL25vmclean.csv")
+) %>%
+  # TODO
+  # recode 0, 1, ., - as distracter, target, other, NA [check in about this]
+  # this leaves NA as NA
+  rename(aoi_old = aoi) %>%
   mutate(aoi = case_when(
     aoi_old == "0" ~ "distractor",
     aoi_old == "1" ~ "target",
     aoi_old == "0.5" ~ "other",
     aoi_old == "." ~ "missing",
     aoi_old == "-" ~ "missing",
-    is.na(aoi_old) ~ "missing"
+    is.na(aoi_old) ~ "missing", # TODO: putting NA here breaks the resampling as some trials only have NA - do we want to handle these cases manually or is "missing" good enough here?
+    TRUE ~ NA,
   )) %>%
-  mutate(t = as.numeric(t)) # ensure time is an integer/ numeric
+  mutate(t = as.numeric(t))
 
-#quick summary
-subj <- d_tidy %>%
-  group_by(age_group,subj,t) %>%
-  summarize(
-    mean_looking=mean(as.numeric(aoi_old),na.rm=T)
-  )
+# quick summary to check the data
+# subj <- d_tidy %>%
+#   group_by(age_group, subj, t) %>%
+#   summarize(
+#     mean_looking = mean(as.numeric(aoi_old), na.rm = T)
+#   )
+#
+# overall <- subj %>%
+#   group_by(age_group, t) %>%
+#   summarize(
+#     avg = mean(mean_looking)
+#   )
+#
+# ggplot(overall, aes(t, avg)) +
+#   geom_hline(yintercept = 0.5, linetype = "dashed") +
+#   geom_line(data = subj, aes(y = mean_looking, group = as.factor(subj)), alpha = 0.05) +
+#   theme(legend.position = "none") +
+#   geom_line() +
+#   facet_wrap(~age_group)
 
-overall <- subj %>%
-  group_by(age_group,t) %>%
-  summarize(
-    avg = mean(mean_looking)
-  )
-
-ggplot(overall, aes(t,avg)) +
-  geom_hline(yintercept=0.5,linetype="dashed")+
-  geom_line(data=subj,aes(y=mean_looking,group=as.factor(subj)),alpha=0.05)+
-  theme(legend.position="none")+
-  geom_line()+facet_wrap(~age_group)
-  
 
 
-
-## 2. Creating the wide.table
-# Populate your wide table from the raw data here
-
-wide.table <- tibble(
-  subject_id = NA,
-  sex = NA,
-  native_language = NA,
-  age = NA,
-  age_units = NA,
-  t = NA,
-  aoi = NA,
-  full_phrase = NA,
-  full_phrase_language = NA,
-  point_of_disambiguation = NA,
-  target_side = NA,
-  condition = NA,
-  vanilla_trial = NA,
-  excluded = NA,
-  exclusion_reason = NA,
-  session_num = NA,
-  sample_rate = NA,
-  tracker = NA,
-  coding_method = NA,
-  target_stimulus_label_original = NA,
-  target_stimulus_label_english = NA,
-  target_stimulus_novelty = NA,
-  target_stimulus_image_path = NA,
-  target_image_description = NA,
-  target_image_description_source = NA,
-  distractor_stimulus_label_original = NA,
-  distractor_stimulus_label_english = NA,
-  distractor_stimulus_novelty = NA,
-  distractor_stimulus_image_path = NA,
-  distractor_image_description = NA,
-  distractor_image_description_source = NA
-) %>%
-  # optional 
+wide.table <- d_tidy %>%
   mutate(
-    # fill out all of these if you have xy data
-    l_x_max = NA,
-    l_x_min = NA,
-    l_y_max = NA,
-    l_y_min = NA,
-    r_x_max = NA,
-    r_x_min = NA,
-    r_y_max = NA,
-    r_y_min = NA,
-    x = NA,
-    y = NA,
-    monitor_size_x = NA,
-    monitor_size_y = NA,
-    # if two subsequent trials can have the same stimuli combination,
-    # use this to indicate the trial order within an administration
-    trial_index = NA,
-    # lab specific name for trials
-    trial_name = NA,
-    # lab specific names for stimuli
-    target_stimulus_name = NA, 
-    distractor_stimulus_name = NA
+    subject_id = subj,
+    native_language = "eng",
+    age = age_group,
+    age_units = "months",
+    full_phrase = NA,
+    full_phrase_language = "eng",
+    point_of_disambiguation = 0,
+    target_side = ifelse(limage == target, "left", "right"),
+    condition = NA,
+    vanilla_trial = FALSE, # TODO Check this!!! there are some inconsistencies here, kreeb and nonce are definitely out, but there is more confusing stuff (look at readme)
+    excluded = ifelse(is.na(includeinfinalanalysis), FALSE, includeinfinalanalysis == "n"),
+    exclusion_reason = ifelse(excluded, "unspecified", NA),
+    session_num = age_group,
+    sample_rate = sampling_rate_hz,
+    tracker = "video camera",
+    coding_method = "manual gaze coding",
+    target_stimulus_label_original = ifelse(target_side == "left", limage, rimage),
+    target_stimulus_label_english = target_stimulus_label_original,
+    target_stimulus_novelty = "familiar",
+    target_stimulus_image_path = NA,
+    target_image_description = target_stimulus_label_original,
+    target_image_description_source = "image path",
+    distractor_stimulus_label_original = ifelse(target_side == "left", rimage, limage),
+    distractor_stimulus_label_english = distractor_stimulus_label_original,
+    distractor_stimulus_novelty = "familiar",
+    distractor_stimulus_image_path = NA,
+    distractor_image_description = distractor_stimulus_label_original,
+    distractor_image_description_source = "image path",
+    trial_index = tr_number
   )
 
 ## 3. Digest the wide.table
@@ -195,29 +140,39 @@ dataset_list <- digest.dataset(
   cite = "Fernald, A., Perfors, A., & Marchman, V. A. (2006). Picking up speed in understanding: Speech processing efficiency and vocabulary growth across the 2nd year.Developmental Psychology, 42(1), 98–116.",
   shortcite = "Fernald et al. (2006)",
   wide.table = wide.table,
-  rezero=TRUE,
-  normalize=TRUE,
-  resample=TRUE
+  rezero = TRUE,
+  normalize = TRUE,
+  resample = TRUE
 )
 
-## 4. Aux Data
-# Add any aux data here - mind that fields like "subject_id" now refer to peekbank internal ids
-# (the external id is now lab_subject_id)
 
-# if you don't have cdi data in your dataset, you can delete this section
-cdi_data <- tibble(
-  subject_id = NA, # this is still referring to the lab subject id
-  instrument_type = NA,
-  language = NA,
-  measure = NA,
-  rawscore = NA,
-  percentile = NA, # can be NA
-  age = NA
-)
+# TODO how to interpret compl, comp etc
+cdi_data <- read_excel(here(data_path, "TLOriginal_CDIScores.xlsx")) %>%
+  rename(subject_id = subj,
+         und12 = und12new,
+         und15 = und15new) %>%
+  select(-child_sex) %>%
+  mutate(across(-subject_id, as.numeric)) %>%
+  pivot_longer(
+    cols = -subject_id,
+    names_to = c("category", "age"),
+    names_pattern = "([^0-9]+)(\\d+)",
+    values_to = "value"
+  ) %>%
+  mutate(
+    language = "English (American)",
+    age = as.numeric(age),
+    valuetype = ifelse(grepl("per", category), "percentile", "rawscore"),
+    measure = ifelse(grepl("voc", category) | grepl("said", category), "prod", "comp"),
+    instrument_type = ifelse(age %in% c(12,15), "wg", "ws") # according to paper
+  ) %>%
+  select(-category) %>% 
+  pivot_wider(
+    names_from = valuetype,
+    values_from = value)
 
-dataset_list[["subjects"]] <- dataset_list[["subjects"]] %>% 
+dataset_list[["subjects"]] <- dataset_list[["subjects"]] %>%
   digest.subject_cdi_data(cdi_data)
 
-## 5. Write and Validate the Data
 
-write_and_validate_list(dataset_list, cdi_expected = FALSE, upload = FALSE)
+write_and_validate_list(dataset_list, cdi_expected = TRUE, upload = FALSE)
