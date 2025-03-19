a215b009c6dd0c85c2236a427fabbf251fe3a3b5
Adrian Steffan <adrian.steffan@hotmail.de>
2025-03-19 18:00:48 +0100

    leftover formatting

--- helper_functions/osf.R
+++ helper_functions/osf.R
@@ -2,9 +2,10 @@ for (package in c("httr", "jsonlite", "glue", "utils", "here", "dplyr")) {
   suppressWarnings(
     suppressPackageStartupMessages(
       library(package, character.only = TRUE)
-      )
     )
+  )
 }
+
 
 get_raw_data_fixed <- function(lab_dataset_id, osf_address = "pr6wu") {
   # drop in replacement for the get_raw_data function of peekbankr (renamed for now)
@@ -44,18 +45,18 @@ upload_osf <- function(lab_dataset_id, osf_address = "pr6wu") {
   if (!file.exists(here(OSF_TOKEN_FILENAME))) {
     stop("To use the upload, please create a osf_token.txt - instructions are found in the README")
   }
-  
+
   DATASET_PATH <- here("data", lab_dataset_id)
   DATASET_PROCESSED_PATH <- here(DATASET_PATH, "processed_data")
-  
-  if(!dir.exists(DATASET_PATH)){
+
+  if (!dir.exists(DATASET_PATH)) {
     stop("Dataset {lab_dataset_id} has no folder")
   }
-  
-  if(!dir.exists(DATASET_PROCESSED_PATH)){
+
+  if (!dir.exists(DATASET_PROCESSED_PATH)) {
     stop("Dataset {lab_dataset_id} has no processed_data_folder")
   }
-  
+
   print(glue("Starting the upload to OSF for dataset {lab_dataset_id}"))
 
   osf_token <- here(OSF_TOKEN_FILENAME) %>%
@@ -78,24 +79,24 @@ upload_osf <- function(lab_dataset_id, osf_address = "pr6wu") {
 
   if (length(existing_processed_data) > 0) {
     print("Existing processed_data folder found, deleting...")
-    invisible(DELETE(existing_processed_data$links$delete, add_headers(Authorization=glue("Bearer {osf_token}"))))
+    invisible(DELETE(existing_processed_data$links$delete, add_headers(Authorization = glue("Bearer {osf_token}"))))
   }
-  
+
   print("Uploading processed_data...")
-  
+
   upload_link <- jsonlite::fromJSON(
     rawToChar(
       PUT(
         glue("{data$links$new_folder}&name=processed_data"),
-        add_headers(Authorization=glue("Bearer {osf_token}"))
-        )$content
+        add_headers(Authorization = glue("Bearer {osf_token}"))
+      )$content
     )
   )[1]$data$links$upload
-  
-  for(f in list.files(DATASET_PROCESSED_PATH)){
+
+  for (f in list.files(DATASET_PROCESSED_PATH)) {
     print(glue("Uploading {f}"))
-    PUT(glue("{upload_link}&name={f}"), body=upload_file(here(DATASET_PROCESSED_PATH, f)), add_headers(Authorization=glue("Bearer {osf_token}")))
+    PUT(glue("{upload_link}&name={f}"), body = upload_file(here(DATASET_PROCESSED_PATH, f)), add_headers(Authorization = glue("Bearer {osf_token}")))
   }
-  
+
   print("Done")
 }
